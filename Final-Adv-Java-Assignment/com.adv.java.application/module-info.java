4ddba9286fc708a2f2c44ccc13dfe7dbc1e316dc
trace7729 <33745132+trace7729@users.noreply.github.com>
2020-02-13 17:40:01 +0800

    Add files via upload

---
+++ Final-Adv-Java-Assignment/com.adv.java.application/module-info.java
@@ -0,0 +1,8 @@
+@SuppressWarnings("module")
+module com.adv.java.application {
+	exports com.adv.java.application;
+	requires com.adv.java.concurrency;
+	requires com.adv.java.iostream;
+	requires com.adv.java.regex;
+	requires com.adv.java.xml;
+}
