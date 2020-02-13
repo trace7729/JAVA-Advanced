172d297c00878d51345c8fc4d845de15d67621cf
trace7729 <33745132+trace7729@users.noreply.github.com>
2020-02-13 17:33:31 +0800

    Add files via upload

---
+++ Lesson 8 Modules/com.horstmann.greet/com/horstmann/greet/Greeter.java
@@ -0,0 +1,8 @@
+package com.horstmann.greet;
+
+public interface Greeter {
+	static Greeter newInstance() {
+		return new com.horstmann.greet.internal.GreeterImpl();
+	}
+	String greet(String subject);
+}
